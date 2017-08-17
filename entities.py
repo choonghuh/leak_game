b5f4fee656d85152dc1c56437df176602e8cc0c8
Choong Huh <choonghuh@gmail.com>
2017-08-16 22:54:50 -0700

    We have events!!! Staffs are becoming unhappy!!

--- entities.py
+++ entities.py
@@ -22,7 +22,7 @@ class Staff:
 	def leak(self, do_it=False):
 		# if loyalty < 50 and has info on something
 		if (self.loyalty<50 or do_it) and len(self.insider_infos)>0:
-			print("Lets see if %s wants to leak")
+			print("Lets see if %s wants to leak" % self.name)
 			pass
 		else:
 			return None
@@ -32,13 +32,6 @@ class Staff:
 			if info.info_id == info_id:
 				return True
 		return False
-
-class Infos:
-	def __init__(self, info_id, summary=None, details=None, severity=None):
-		self.info_id = info_id
-		self.summary = summary
-		self.details = details
-		self.published = False
 
 
 class StaffList:
@@ -51,13 +44,14 @@ class StaffList:
 			self.staff_list.append(Staff(namestr, titlestr, staff_id))
 
 	def overview(self):
+		# TODO - Want to display loyalty only if spied or researched
 		if len(self.staff_list) == 0:
 			return
 		for staff in self.staff_list:
 			if not staff.fired:
-				print("%s - %s - ID:%d - %d/100" % \
+				print("%s - %s - ID:%d - %d/100 - Info: %d" % \
 					(staff.name, staff.title,\
-					staff.id, staff.loyalty))
+					staff.id, staff.loyalty, len(staff.insider_infos)))
 
 	def fire(self, sid):
 		exists = False
@@ -66,6 +60,61 @@ class StaffList:
 				staff.fired = True
 				exists = True
 				print("Fired %s" % staff.name)
+
+	def process_info(self, info_id, details):
+		# detail should be dict with key 'name' and 'severity'. Raise KeyError maybe
+		new_info = Info(info_id, details['name'], details['severity'])
+
+		count = 0
+		for staff in self.staff_list:
+			if random.randint(0,4) % 4 == 0: # For now, 25% chance
+				staff.insider_infos.append(new_info)
+				count += 1
+				# TODO - If NOT A SPY, decrease loyalty by SOME amount.
+				staff.decrease_loyalty(details['severity']*random.randint(0,5))
+
+		print("%d staff(s) found out about this event." % count)
+
+
+class President:
+	class PresidentAction:
+		def __init__(self, name, likelihood, severity):
+			self.name = name
+			self.likelihood = likelihood
+			self.severity = severity
+
+
+	def __init__(self):
+		self.available_actions = [
+			self.PresidentAction("doing nothing", 10, 0),
+			self.PresidentAction("chatting with Vladimir Putin", 1, 2),
+			self.PresidentAction("taking bribes", 1, 2),
+			self.PresidentAction("yelling racial profanity at staff", 2, 1),
+			self.PresidentAction("yelling homophobic profanity at staff", 2, 1),
+			self.PresidentAction("chatting with an ex-girlfriend", 1, 1),
+			self.PresidentAction("on a drunken rant", 1, 1),
+		]
+
+	# Return an Info / None?
+	def do_something(self):
+		temp_action_list = []
+		for action in self.available_actions:
+			for aa in xrange(0, action.likelihood):
+				temp_action_list.append({"name":action.name, "severity":action.severity})
+
+		choice = temp_action_list[random.randint(0,len(temp_action_list)-1)]
+		print("\nPresident is " + choice["name"] + "...\n")
+		if choice["severity"] != 0:
+			# return {"name":choice.name, "severity":choice.severity}
+			return choice
+		return None
+
+class Info:
+	def __init__(self, info_id, summary=None, severity=None):
+		self.info_id = info_id
+		self.summary = summary
+		# self.details = details
+		self.published = False
 
 
 class Leak:
--- maingame.py
+++ maingame.py
@@ -1,10 +1,8 @@
-import sys
+import sys, time, random
 
 from datetime import datetime
 
-from entities import StaffList, Director, Leak
-
-import random
+from entities import StaffList, Director, President, Leak
 
 class Game:
 	def __init__(self, namestr):
@@ -13,6 +11,7 @@ class Game:
 		# Entities
 		self.director = Director(namestr)
 		self.staff_list = StaffList()
+		self.president = President()
 
 		self.action_list = [
 			self.view_status,
@@ -31,8 +30,8 @@ class Game:
 
 	def fire_staff(self):
 		# Need to take another raw input
-		eid = int(raw_input("Enter the Staff ID of the staff member to be fired: "))
-		self.staff_list.fire(eid)
+		staff_id = int(raw_input("Enter the Staff ID of the staff member to be fired: "))
+		self.staff_list.fire(staff_id)
 		pass
 
 	def motd(self):
@@ -47,27 +46,46 @@ class Game:
 
 		self.todays_leaks = []
 
+	def process_president(self):
+		print("\n\nPresident is thinking....\n")
+		time.sleep(3)
+		something = self.president.do_something()
+		time.sleep(3)
+		if something:
+			# TODO - determine which staff will know about this. For now, all?
+			# Day is OK as a unique Info ID as long as there is only one per day
+			self.staff_list.process_info(self.day, something)
+
+
 	def print_actions(self):
 		print("1 - View your current status and history.")
 		print("2 - View the staff list")
 		print("3 - Fire a staff")
 		print("4 - End the day")
+		# Use a skill?
+		# Use item or money to silence a leak, convince a staff, etc.?
 		pass
 
 	def pass_day(self):
+		# President randomly does dumb stuff here
+		self.process_president()
+
+
 		#leak here
 		for staff in self.staff_list.staff_list:
 			leak = staff.leak()
 			if leak is not None:
 				self.todays_leaks.append(leak)
 		self.day += 1
-		# TODO - President needs to do dumb stuff here and create an Info
 
 	def playgame(self):
 		print("Let's do it")
 		while self.director.respect>0:
+			# Start of the day. Display Messages Of The Day
 			self.motd()
 			# TODO - self.spy_overview() # Some spy that tells you who knows what info
+
+			# Display a list of available actions
 			self.print_actions()
 			action = int(raw_input())#Check Input
 			while action != 4:
